8349e8c086abced39ab0859cc2d8dab900a68a44
Johannes Stein <johannesstein@freeze-dev.com>
2014-01-19 17:26:09 +0100

    Added scene class

---
+++ Classes/Scene.h
@@ -0,0 +1,19 @@
+//
+//  Scene.h
+//  PirateGame
+//
+//  Created by Johannes Stein on 06/01/14.
+//
+//
+
+#import "SPSprite.h"
+
+@interface Scene : SPSprite
+
+@property SPSprite* guiLayer;
+@property NSString* name;
+@property id director;
+
+-(id) initWithName:(NSString *)name;
+
+@end
---
+++ Classes/Scene.m
@@ -0,0 +1,32 @@
+//
+//  Scene.m
+//  PirateGame
+//
+//  Created by Johannes Stein on 06/01/14.
+//
+//
+
+#import "Scene.h"
+
+@implementation Scene
+
+-(id) init
+{
+    if ((self = [super init])) {
+        self.guiLayer = [[SPSprite alloc] init];
+        self.director = nil;
+        self.name = @"scene";
+    }
+    
+    return self;
+}
+
+-(id) initWithName:(NSString *) name
+{
+    self = [self init];
+    self.name = name;
+    
+    return self;
+}
+
+@end
