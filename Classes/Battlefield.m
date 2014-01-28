4f32406d2531cce50a931d30cee962cfee126a9e
Johannes Stein <johannesstein@freeze-dev.com>
2014-01-28 19:09:35 +0100

    Moving the ship to the current touch position

--- Classes/Battlefield.m
+++ Classes/Battlefield.m
@@ -11,6 +11,23 @@
 
 @implementation Battlefield
 
+-(void) onBackgroundTouch: (SPTouchEvent*) event
+{
+    SPTouch* touch = [[event touchesWithTarget:self andPhase:SPTouchPhaseBegan] anyObject];
+    
+    if (touch) {
+        SPTween* tweenX = [SPTween tweenWithTarget:_pirateShip time:2.0f];
+        SPTween* tweenY = [SPTween tweenWithTarget:_pirateShip time:2.0f];
+        
+        
+        [tweenX animateProperty:@"x" targetValue:touch.globalX - (_pirateShip.width / 2)];
+        [tweenY animateProperty:@"y" targetValue:touch.globalY - (_pirateShip.height / 2)];
+        
+        [Sparrow.juggler addObject:tweenX];
+        [Sparrow.juggler addObject:tweenY];
+    }
+}
+
 -(id) init
 {
     if ((self = [super init])) {
@@ -18,16 +35,18 @@
         background.x = (Sparrow.stage.width - background.width) / 2;
         background.y = (Sparrow.stage.height - background.height) / 2;
         
-        SPImage *pirateShip = [SPImage imageWithTexture:[Assets texture:@"ship_pirate.png"]];
-        pirateShip.x = (Sparrow.stage.width - pirateShip.width) / 2;
-        pirateShip.y = (Sparrow.stage.height - pirateShip.height) / 2;
+        _pirateShip = [SPImage imageWithTexture:[Assets texture:@"ship_pirate.png"]];
+        _pirateShip.x = (Sparrow.stage.width - _pirateShip.width) / 2;
+        _pirateShip.y = (Sparrow.stage.height - _pirateShip.height) / 2;
         
-        SPImage *ship = [SPImage imageWithTexture:[Assets texture:@"ship.png"]];
+        SPImage* ship = [SPImage imageWithTexture:[Assets texture:@"ship.png"]];
         ship.x = 100;
         ship.y = 100;
         
+        [background addEventListener:@selector(onBackgroundTouch:) atObject:self forType:SP_EVENT_TYPE_TOUCH];
+        
         [self addChild:background];
-        [self addChild:pirateShip];
+        [self addChild:_pirateShip];
         [self addChild:ship];
     }
     
